30151698e6387bcbab440da8a1bd100db93037ac
GodJungGun <jg0428.lim@gmail.com>
2017-09-05 11:54:11 +0900

    CREATE: edit page & update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'homes#index'
 
   get 'products/raw' => 'products#raw'
+  get 'products/:id/edit' => 'products#edit', as: :count_edit
+
+  post 'products/:id/update' => 'products#update', as: :count_update
 
   resources :products
   resources :homes
