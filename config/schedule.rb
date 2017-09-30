2e6f1aedaf99472c868f4dd8996bbefa76cccac9
GodJungGun <jg0428.lim@gmail.com>
2017-09-30 14:55:11 +0900

    MODIFY: whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,44 +18,14 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-require 'mechanize'
-require 'json'
-require 'date'
+# set :environment, "development"
+set :output, {error: "log/cron_error_log.log", standard: "log/cron_log.log"}
 
-every 1.min do
+every 1.minutes do
   puts "Whenever woking~!!"
+  File.open("log/test.log", 'w') { |file| file.write("test") }
 end
 
-every 1.day, at: '5:30 pm', tz: 'Seoul' do
-  mechanize = Mechanize.new
-  login = mechanize.get('https://www.901giji.com/scm/index.php/member/login')
-
-  #look for the wanted form
-  form = login.form_with action: "https://www.901giji.com/scm/index.php/member/login_chk"
-  form.field_with(name: "id").value = ENV['SCM_ID']
-  form.field_with(name: "password").value = ENV['SCM_PASSWORD']
-  form.submit
-
-  scm = mechanize.get("https://www.901giji.com/scm/index.php/product/search")
-
-  @data = {}
-  count = scm.parser.css("table.table tbody tr").count
-
-  for i in 1..count do
-    @data[scm.parser.css("table.table tbody tr[#{i}] td[2]").text.to_sym] = scm.parser.css("table.table tbody tr[#{i}] td[8]").text.tr(',','').to_i
-  end
-
-  Product.all.each do |p|
-    count = @data[:"#{p.code}"]
-
-    if Count.where(product_id: p.id).where(date: (Date.today.in_time_zone("Seoul") - 1).to_s).take.present?
-      if count > Count.where(product_id: p.id).where(date: (Date.today.in_time_zone("Seoul") - 1).to_s).take.count
-        Count.create(count: count, product_id: p.id, date: Date.today.in_time_zone("Seoul"), goods: true)
-      else
-        Count.create(count: count, product_id: p.id, date: Date.today.in_time_zone("Seoul"), goods: false)
-      end
-    else
-      Count.create(count: count, product_id: p.id, date: Date.today.in_time_zone("Seoul"), goods: true)
-    end
-  end
+every 1.day, at: '5:00 pm', tz: 'Seoul' do
+  runner "Product.whenever_create"
 end
