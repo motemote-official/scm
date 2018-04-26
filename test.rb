c5e849e29156838f72098fa894298bca8d5bba98
GodJungGun <jg0428.lim@gmail.com>
2018-04-26 18:31:15 +0900

    FIX: apply show page

--- test.rb
+++ test.rb
@@ -1,1 +1,18 @@
-File.open("log/test.log", 'a') { |file| file.write("test") }
+#File.open("log/test.log", 'a') { |file| file.write("test") }
+#class ApplicationController < ActionController::Base
+
+  #last_id = Attend.last.id
+
+  #Attend.all.each do |a|
+    #id = a.id
+
+    #unless id == last_id
+      #unless Attend.find(id+1).nil?
+        #b = Attend.find(id+1)
+        #if a.rocket_member_id == b.rocket_member_id && a.date == b.date
+          #b.destroy
+        #end
+      #end
+    #end
+  #end
+#end
